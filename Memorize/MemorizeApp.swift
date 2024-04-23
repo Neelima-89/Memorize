2a76ad3f97f35ebeb88af3207e69e22f88167d06
Neelima-89 <neelimadarapureddy@gmail.com>
2024-04-23 13:41:58 +0530

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Neelima on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
